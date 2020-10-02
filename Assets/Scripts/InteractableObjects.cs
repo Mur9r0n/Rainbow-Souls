f83bb94ae03dc6c893349b00c14013dcdc80ffe7
Christophe Schmit <schmit.christophe@hotmail.com>
2020-10-02 18:20:22 +0200

    Tested Dissolve shader on Chest

--- Assets/Scripts/InteractableObjects.cs
+++ Assets/Scripts/InteractableObjects.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,9 +15,36 @@ public class InteractableObjects : MonoBehaviour
     };
 
     public Type m_Type;
+    public bool m_Dissolving = false;
+    public Material m_Material;
+
     void Start()
     {
         InteractManager.Instance.AddToList(this);
+        if (m_Type == Type.Chest)
+        {
+            m_Material = GetComponent<Renderer>().material;
+        }
+    }
+
+    private void Update()
+    {
+        //Still Testing
+        #region Testing
+        if (m_Dissolving)
+        {
+            float temp = m_Material.GetFloat("Vector1_8DEAC01A");
+            if (temp <= 1)
+            {
+                m_Material.SetFloat("Vector1_8DEAC01A", temp += Time.deltaTime);
+            }
+            else
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        #endregion
+            
     }
 
     public void Use()
@@ -25,32 +53,31 @@ public class InteractableObjects : MonoBehaviour
         switch (m_Type)
         {
             case Type.Checkpoint:
-            
+
                 Debug.Log("Activate Checkpoint");
                 break;
-            
+
             case Type.Chest:
-            
+
                 Debug.Log("Open Chest");
                 InteractManager.Instance.RemoveFromList(this);
+                m_Dissolving = true;
                 break;
-            
+
             case Type.Door:
-            
+
                 Debug.Log("Open Door");
                 break;
-            
+
             case Type.Item:
-            
+
                 Debug.Log("Pick up Item");
                 InteractManager.Instance.RemoveFromList(this);
                 break;
-            
+
             case Type.NPC:
                 Debug.Log("Talk To NPC");
                 break;
-
         }
-        
     }
 }
