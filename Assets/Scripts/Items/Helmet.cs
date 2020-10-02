99739c0a8dae16f964326f52b1c71f16cb485d1f
Ralf <ralfvb97@gmail.com>
2020-10-02 21:12:03 +0200

    Created some Scriptable Objects

---
+++ Assets/Scripts/Items/Helmet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Helmet", menuName = "Item/Helmet")]
+public class Helmet : Item
+{
+    [Header("Specific Item Informations")] [SerializeField, Tooltip("Armor Value")]
+    private int m_armorValue;
+
+    [SerializeField, Tooltip("Bleeding Resistance")]
+    private int m_bleedingResistance;
+
+    [SerializeField, Tooltip("Poison Resistance")]
+    private int m_poisonResistance;
+}
