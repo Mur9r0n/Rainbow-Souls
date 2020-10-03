d75885a268935fcabf29832a5d0eb3ea320d35e3
Christophe Schmit <schmit.christophe@hotmail.com>
2020-10-03 20:51:55 +0200

    Simple State Machine working for sheeeep

--- Assets/Scripts/KI/Sheep/SheepController.cs
+++ Assets/Scripts/KI/Sheep/SheepController.cs
@@ -4,15 +4,13 @@ using UnityEngine.AI;
 
 public class SheepController : MonoBehaviour
 {
-    #region Components and Vectors
-
     public NavMeshAgent m_Agent;
     public Vector3? TargetPosition { get; set; }
     public Vector3 OriginalPosition { get; set; }
     public Quaternion OriginalRotation { get; set; }
-
-    #endregion
-
+    public float OriginalFOVAngle { get; set; }
+    public float OriginalFOVDistance { get; set; }
+    
     [SerializeField, Tooltip("Maximum Healthpoints.")]
     private float m_maxHealthPoints;
 
@@ -23,11 +21,11 @@ public class SheepController : MonoBehaviour
     private float m_attackDistance;
 
     [SerializeField, Tooltip("Field of View Distance."), Range(1f, 100f)]
-    private float m_fovDistance = 1f;
+    public float m_FOVDistance = 1f;
 
     [SerializeField, Tooltip("Field of View Angle."), Range(0f, 90f)]
-    private float m_fovAngle = 1f;
-
+    public float m_FOVAngle = 1f; 
+    
     private ABaseState m_activeState;
     private SheepIdleState m_idleState;
 
@@ -36,35 +34,53 @@ public class SheepController : MonoBehaviour
         m_Agent = GetComponent<NavMeshAgent>();
         OriginalPosition = transform.position;
         OriginalRotation = transform.rotation;
+        OriginalFOVAngle = m_FOVAngle;
+        OriginalFOVDistance = m_FOVDistance;
 
         m_idleState = new SheepIdleState();
         SheepAttackState m_attackState = new SheepAttackState();
-        // SheepResetState m_resetState = new SheepResetState();
+        SheepResetState m_resetState = new SheepResetState();
         SheepWalkState m_walkState = new SheepWalkState();
 
         m_idleState.SheepInit(this, new KeyValuePair<ABaseState.TransitionDelegate, ABaseState>
             (
-                () => Test(), m_walkState
+                () => PlayerInFOV() && PlayerInRange(), m_attackState
             ),
             new KeyValuePair<ABaseState.TransitionDelegate, ABaseState>
             (
-                () => PlayerInFOV(), m_attackState
+                () => PlayerInFOV() && !PlayerInRange(), m_walkState
             ));
 
         m_attackState.SheepInit(this, new KeyValuePair<ABaseState.TransitionDelegate, ABaseState>
-        (
-            () => Test(), m_idleState
-        ));
-        //
-        // m_resetState.SheepInit(this, new KeyValuePair<ABaseState.TransitionDelegate, ABaseState>
-        // (
-        //     
-        // ));
-        //
+            (
+                () => PlayerInFOV() && !PlayerInRange(), m_walkState
+            ),
+            new KeyValuePair<ABaseState.TransitionDelegate, ABaseState>
+            (
+                () => !PlayerInFOV() && !PlayerInRange(), m_resetState
+            ));
+
+        m_resetState.SheepInit(this, new KeyValuePair<ABaseState.TransitionDelegate, ABaseState>
+            (
+                () => m_resetState.IsFinished, m_idleState
+            ),
+            new KeyValuePair<ABaseState.TransitionDelegate, ABaseState>
+            (
+                () => PlayerInFOV() && PlayerInRange(), m_attackState
+            ),
+            new KeyValuePair<ABaseState.TransitionDelegate, ABaseState>
+            (
+                () => PlayerInFOV() && !PlayerInRange(), m_walkState
+            ));
+
         m_walkState.SheepInit(this, new KeyValuePair<ABaseState.TransitionDelegate, ABaseState>
-        (
-            () => Test(), m_idleState
-        ));
+            (
+                () => !PlayerInFOV() && !PlayerInRange(), m_resetState
+            ),
+            new KeyValuePair<ABaseState.TransitionDelegate, ABaseState>
+            (
+                () => PlayerInFOV() && PlayerInRange(), m_attackState
+            ));
 
         m_activeState = m_idleState;
     }
@@ -104,14 +120,14 @@ public class SheepController : MonoBehaviour
                                     origin;
         // Debug.Log(Vector3.SignedAngle(dir, transform.forward, Vector3.forward));
 
-        if (Vector3.SignedAngle(directionToPlayer, transform.forward, Vector3.forward) <= m_fovAngle &&
-            Vector3.SignedAngle(directionToPlayer, transform.forward, Vector3.forward) >= -m_fovAngle)
+        if (Vector3.SignedAngle(directionToPlayer, transform.forward, Vector3.forward) <= m_FOVAngle &&
+            Vector3.SignedAngle(directionToPlayer, transform.forward, Vector3.forward) >= -m_FOVAngle)
         {
             // Debug.Log("Player in FOV!");
             RaycastHit hit;
-            if (Vector3.Distance(origin, playerposition) <= m_fovDistance)
+            if (Vector3.Distance(origin, playerposition) <= m_FOVDistance)
             {
-                if (Physics.Raycast(origin, directionToPlayer, out hit, m_fovDistance))
+                if (Physics.Raycast(origin, directionToPlayer, out hit, m_FOVDistance))
                 {
                     if (hit.collider.gameObject.CompareTag("Player"))
                     {
@@ -122,7 +138,7 @@ public class SheepController : MonoBehaviour
                     }
                     else
                     {
-                        Debug.Log("Hit something else! " );
+                        Debug.Log("Hit something else! ");
                         Debug.DrawRay(origin, directionToPlayer, Color.red, 5f);
                         return false;
                     }
@@ -133,18 +149,27 @@ public class SheepController : MonoBehaviour
         return false;
     }
 
-    private bool Test()
+    private bool PlayerInRange()
     {
-        return m_currentHealthPoints < 10000000f;
+        Vector3 playerposition = GameManager.Instance.PlayerTransform.position;
+        Vector3 origin = transform.position + new Vector3(0, 1, 0);
+
+        if (Vector3.Distance(playerposition, origin) <= m_attackDistance)
+        {
+            return true;
+        }
+
+        return false;
     }
+
 
     private void OnDrawGizmos()
     {
-        Vector3 FovLine1 = Quaternion.AngleAxis(m_fovAngle, transform.up) * transform.forward * m_fovDistance;
-        Vector3 FovLine2 = Quaternion.AngleAxis(-m_fovAngle, transform.up) * transform.forward * m_fovDistance;
+        Vector3 FovLine1 = Quaternion.AngleAxis(m_FOVAngle, transform.up) * transform.forward * m_FOVDistance;
+        Vector3 FovLine2 = Quaternion.AngleAxis(-m_FOVAngle, transform.up) * transform.forward * m_FOVDistance;
 
         Gizmos.color = Color.magenta;
-        Gizmos.DrawWireSphere(transform.position, m_fovDistance);
+        Gizmos.DrawWireSphere(transform.position, m_FOVDistance);
 
         Gizmos.color = Color.black;
         Gizmos.DrawRay(transform.position, FovLine1);
--- Assets/Scripts/KI/Sheep/SheepIdleState.cs
+++ Assets/Scripts/KI/Sheep/SheepIdleState.cs
@@ -1,7 +1,18 @@
-﻿public class SheepIdleState : ABaseState
+﻿using UnityEngine;
+
+public class SheepIdleState : ABaseState
 {
+    public override bool Enter()
+    {
+        //TODO Adjustments todo
+        m_controller.m_FOVAngle = m_controller.OriginalFOVAngle;
+        m_controller.m_FOVDistance = m_controller.OriginalFOVDistance;
+        Debug.Log("Hier wäre die Update! SheepIdleState");
+        return base.Enter();
+    }
+
     public override void Update()
     {
-        // IsFinished = true;
+
     }
 }
