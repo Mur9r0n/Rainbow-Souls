8f55c12a1dc4807cd7e8cfdb6f7440846c650d25
Christophe Schmit <schmit.christophe@hotmail.com>
2020-10-13 16:06:45 +0200

    Fixed Rotation by Jan

--- Assets/Scripts/KI/Sheep/SheepResetState.cs
+++ Assets/Scripts/KI/Sheep/SheepResetState.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class SheepResetState : ABaseState
 {
+    private Quaternion? temp = null;
+    private float timer = 0;
     public override bool Enter()
     {
         m_controller.m_Agent.isStopped = false;
@@ -15,10 +17,21 @@ public class SheepResetState : ABaseState
     {
         if (Vector3.Distance(m_controller.transform.position, m_controller.OriginalPosition) <= .2f)
         {
+            timer += Time.deltaTime;
+            if (timer > 1)
+            {
+                m_controller.transform.rotation = m_controller.OriginalRotation;
+                IsFinished = true;
+            }
+            if (temp == null)
+            {
+                temp = m_controller.transform.rotation;
+            }
             m_controller.m_Agent.enabled = false;
-            m_controller.transform.rotation = m_controller.OriginalRotation;
+            // m_controller.transform.rotation = m_controller.OriginalRotation;
+            m_controller.transform.rotation = Quaternion.Lerp((Quaternion)temp,m_controller.OriginalRotation,timer);
             m_controller.m_Agent.enabled = true;
-            IsFinished = true;
+            // IsFinished = true;
         }
     }
 
