dad84e634f09d432f5ced507e8d4a826ab50e3db
Christophe Schmit <schmit.christophe@hotmail.com>
2021-01-27 20:48:09 +0100

    Try

--- Assets/Scripts/MainMenuBehaviours.cs
+++ Assets/Scripts/MainMenuBehaviours.cs
@@ -10,6 +10,7 @@ using Slider = UnityEngine.UI.Slider;
 
 public class MainMenuBehaviours : MonoBehaviour
 {
+    //PUSH COMMAND
     [SerializeField] private Button m_continueButton;
     [SerializeField] private Button m_loadGameBackButton;
 
